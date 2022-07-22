218e69104574458eb0444863c4f31cc5092062d8
kiaarablu <109739665+kiaarablu@users.noreply.github.com>
2022-07-22 10:54:28 -0700

    Add files via upload

---
+++ StackArrey2/StackArrey2/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackArrey2
+{
+    internal class Program
+    {
+        public int[] StckArray;
+        public int top;
+        static void Main(string[] args)
+        {
+        }
+        bool isfull()
+        {
+            if (top >= StckArray.Length)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        
+        bool push (int a)
+        {
+            if (!isfull())
+            {
+                top++;
+                a = StckArray[top];
+                return true;
+            }
+            else
+            {
+               return false;
+            }
+        }
+        bool pop()
+        {
+            if (!isfull())
+            {
+                Console.WriteLine(StckArray[top]);
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("i cant");
+                return false;
+            }
+
+        }
+        bool pick()
+        {
+            if(!isfull())
+            {
+                Console.WriteLine("table is empty");
+                return false;
+
+            }
+            else
+            {
+                Console.WriteLine(StckArray[top]);
+                return true;
+            }
+
+        }   
+
+
+    }
+}
